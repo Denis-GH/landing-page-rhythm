2542d3de6ce7f312be269243a0d6512729e2f63d
Denis Vakulenko <00vakulenko@gmail.com>
2024-01-05 23:00:12 +0500

    firs version

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-reviews', {
+    // Optional parameters
+    autoHeight: true,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: 'true',
+    },
+  });
